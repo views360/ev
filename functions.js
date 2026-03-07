f78212cbaefe26f0de7dfd8b64fabaa1e226dfdc
Views 360 <120094358+views360@users.noreply.github.com>
2026-03-07 08:28:41 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -554,7 +554,7 @@ function calculate() {
     const conclusionsBox = document.getElementById("conclusionsBox");
     if (providers.length > 0) {
         const bestProvider = providers[0];
-        const timeLine = `Approximate driving time for ${(inputs.journeyMiles} miles at 60mph is <strong>${(inputs.journeyMiles / 60).toFixed(1)} hours</strong>.`;
+        const timeLine = `<p style="font-size: 1.1rem">Approximate driving time for ${(inputs.journeyMiles} miles at 60mph is <strong>${(inputs.journeyMiles / 60).toFixed(1)} hours</strong></p>.`;
         
         const minSpeedSelect = document.getElementById("minSpeed");
         const minSpeedLabel = minSpeedSelect.options[minSpeedSelect.selectedIndex].text;
