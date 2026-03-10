eed13eb3bafd5d3a02975782fdfc6f6b85651372
Views 360 <120094358+views360@users.noreply.github.com>
2026-03-10 08:58:02 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -461,10 +461,10 @@ function calculate() {
     document.getElementById("preChargeLine").innerHTML = `<h3>PAYG SUMMARY</h3>Pre-journey starting charge: <strong>${startChargeKwh.toFixed(1)} kWh</strong> (£${startChargeCost.toFixed(2)})`;
     document.getElementById("homeRangeLine").innerHTML = `<div class="tooltip-container">
                             <span class="info-icon" style="font-size:0.8rem" onclick="toggleTooltip(this)">💡</span>
-                          <div class="tooltip-box tooltip-down tooltip-reset-offset">This is the distance you can expect to drive before your first public charge along your route.</div>Range from pre-journey starting charge: <strong>${initialRange.toFixed(0)} miles</strong>`;
+                          <div class="tooltip-box tooltip-down tooltip-right">This is the distance you can expect to drive before your first public charge along your route.</div>Range from pre-journey starting charge: <strong>${initialRange.toFixed(0)} miles</strong>`;
     document.getElementById("publicMilesLine").innerHTML = `<div class="tooltip-container">
                             <span class="info-icon" style="font-size:0.8rem" onclick="toggleTooltip(this)">💡</span>
-                          <div class="tooltip-box tooltip-down tooltip-reset-offset">This is how many miles of your trip will need to be covered by public charging.</div>PAYG public charging miles needed: <strong>${publicMiles.toFixed(0)} miles</strong> `;
+                          <div class="tooltip-box tooltip-down tooltip-right">This is how many miles of your trip will need to be covered by public charging.</div>PAYG public charging miles needed: <strong>${publicMiles.toFixed(0)} miles</strong> `;
     document.getElementById("publicKwhLine").innerHTML = `PAYG public charging energy needed: <strong>${publicKwh.toFixed(1)} kWh @ ${inputs.adhoc}p</strong>`;
     document.getElementById("adhocCostLine").innerHTML = `Total journey cost (pre-charge + standard PAYG): <strong>£${totalAdhocCost.toFixed(2)}</strong>`;
     
@@ -1094,4 +1094,3 @@ function closeBeReminder() {
         }, 400); 
     }
 }
-
