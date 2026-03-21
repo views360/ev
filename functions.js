20c855437427b9d64f9f78ef90e49a5f5d5740d9
Views 360 <120094358+views360@users.noreply.github.com>
2026-03-21 21:09:06 +0000

    Update functions.js

--- functions.js
+++ functions.js
@@ -753,11 +753,11 @@ function calculate() {
                 <h3>RESULTS CONTENTS</h3>
                 <ul style="margin:0; padding-left:20px; font-size:0.95rem;">
                     <li><a href="#payg-summary" style="color: var(--accent); text-decoration:none;">PAYG Summary</a></li>
-                    <li><a href="#providerResults" style="color: var(--accent); text-decoration:none;">Table of Results</a></li>
+                    <li><a href="#providerResults" style="color: var(--accent); text-decoration:none;">Providers & Subscriptions</a></li>
                     <li><a href="#payg-vs-subscription" style="color: var(--accent); text-decoration:none;">PAYG vs Subscription Conclusion</a></li>
                     <li><a href="#charging-times-section" style="color: var(--accent); text-decoration:none;">Charging Times</a></li>
                     <li><a href="#real-world-assessment" style="color: var(--accent); text-decoration:none;">Real-World Charging Itinerary</a></li>
-                    <li><a href="#graph-section" style="color: var(--accent); text-decoration:none;">Graph</a></li>
+                    <li><a href="#graph-section" style="color: var(--accent); text-decoration:none;">Subscriptions Break-Even Graph</a></li>
                 </ul>
             </div>
         `;
